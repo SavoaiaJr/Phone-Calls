468b7640b10a323184b47d6e87e15d1edf08ddfe
Madalin Savoaia <madalin.savoaia@endava.com>
2019-05-25 23:52:20 +0300

    Make remove schedule invisible.

--- app/src/main/java/com/sia4/phonecalls/CallScheduleReceiver.java
+++ app/src/main/java/com/sia4/phonecalls/CallScheduleReceiver.java
@@ -14,12 +14,15 @@ public class CallScheduleReceiver extends BroadcastReceiver {
 
         DataBaseManager dbManager = new DataBaseManager(context);
         ScheduleActivity.Schedule schedule = dbManager.getSchedules().get(id);
-        dbManager.removeSchedule(schedule);
 
-        String name = intent.getStringExtra(CallScheduleManager.NAME_KEY);
-        String phone = intent.getStringExtra(CallScheduleManager.PHONE_KEY);
+        if (schedule != null) {
+            dbManager.removeSchedule(schedule);
 
-        Toast.makeText(context, "Calling " + name, Toast.LENGTH_SHORT).show();
-        PhoneManager.getInstance().call(phone, context);
+            String name = intent.getStringExtra(CallScheduleManager.NAME_KEY);
+            String phone = intent.getStringExtra(CallScheduleManager.PHONE_KEY);
+
+            Toast.makeText(context, "Calling " + name, Toast.LENGTH_SHORT).show();
+            PhoneManager.getInstance().call(phone, context);
+        }
     }
 }
